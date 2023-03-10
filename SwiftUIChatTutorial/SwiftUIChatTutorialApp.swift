6db7b5fa69d4695afcd09b90646f51350af5f57c
JaroslavSku <jaroslavskudrna@gmail.com>
2023-03-10 13:28:23 +0100

    Initial Commit

---
+++ SwiftUIChatTutorial/SwiftUIChatTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatTutorialApp.swift
+//  SwiftUIChatTutorial
+//
+//  Created by MacBook on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
